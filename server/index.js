154a98a4796f391b3f168fd09582e23e804d33e1
scie <emabicova@seznam.cz>
2025-05-13 10:00:37 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -15,14 +15,20 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Middleware to handle CORS and JSON parsing
+// app.use(cors({
+//     origin: process.env.CLIENT_URL, // frontend's URL
+//     credentials: true, // Allow cookies and credentials
+// }));
 app.use(cors({
-    origin: process.env.CLIENT_URL, // frontend's URL
+    origin: 'https://tqycompanion.netlify.app', // Allow requests from your frontend
     credentials: true, // Allow cookies and credentials
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
 }));
 app.use(express.json()); // parse JSON request bodies
 app.use(express.urlencoded({ extended: true })); // parse URL-encoded bodies
 
-
+app.options('*', cors()); // Enable pre-flight requests for all routes
 app.use('/users', userRoutes);
 app.use('/prompts', promptRoutes);
 app.use('/game', gameRoutes);
